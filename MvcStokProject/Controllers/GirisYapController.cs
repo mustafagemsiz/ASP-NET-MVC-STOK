a81203a8bd53d863e64f4972ee409e7279c7ae1e
Mustafa Gemsiz <birhatgemsiz@hotmail.com>
2021-09-07 16:09:50 +0300

    finish

---
+++ MvcStokProject/Controllers/GirisYapController.cs
@@ -0,0 +1,46 @@
+﻿using MvcStokProject.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcStokProject.Controllers
+{
+    [AllowAnonymous]
+    public class GirisYapController : Controller
+    {
+        
+        Db_StokEntities db = new Db_StokEntities();
+        // GET: GirisYap
+        public ActionResult Giris()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Giris(TBL_ADMIN p)
+        {
+            var bilgi = db.TBL_ADMIN.FirstOrDefault(x => x.KULLANICI == p.KULLANICI && x.SIFRE == p.SIFRE);
+            if (bilgi != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgi.KULLANICI, false);
+                return RedirectToAction("Index", "Musteri");
+            }
+            else
+            {
+                return View();
+            }
+
+           
+        }
+
+        public ActionResult Cikis()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Giris", "GirisYap");
+        }
+    }
+}
--- MvcStokProject/Global.asax.cs
+++ MvcStokProject/Global.asax.cs
@@ -11,6 +11,7 @@ namespace MvcStokProject
     {
         protected void Application_Start()
         {
+            GlobalFilters.Filters.Add(new AuthorizeAttribute());
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
